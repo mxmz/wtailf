99fd0d98aec408ab3ac61431e53a0d0b5614e6e3
Maxim <minimal.oasis@gmail.com>
2020-08-09 16:13:10 +0000

    Update netinterfaces.go

--- go/util/netinterfaces.go
+++ go/util/netinterfaces.go
@@ -23,7 +23,7 @@ func GetNetInterfaceAddresses() []InterfaceAddress {
 	var rv = []InterfaceAddress{}
 IFACES:
 	for _, iface := range interfaces {
-		//fmt.Printf("%s %v\n", iface.Name, iface.Addrs)
+		fmt.Printf("%s %v\n", iface.Name, iface.Addrs)
 		for _, toSkip := range []string{"lo", "docker", "tun", "vpn"} {
 			if strings.HasPrefix(iface.Name, toSkip) {
 				continue IFACES
